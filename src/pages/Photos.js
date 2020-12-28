566d89e1429c90bd252e2a784ab4aeec134e632b
pabloa1311 <pabloa1311@gmail.com>
2020-12-28 15:01:38 -0500

    Heart and Fav Icons added

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, {useContext} from "react"
+
+import Image from "../components/Image.js"
+import {Context} from "../Context.js"
+import {getClass} from "../utils"
 
 function Photos() {
+    const {allPhotos} = useContext(Context)
+    
+    const imageElements = allPhotos.map((img, i) => (
+        <Image key={img.id} img={img} className={getClass(i)} />
+    ))
+    
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {imageElements}
         </main>
     )
 }
