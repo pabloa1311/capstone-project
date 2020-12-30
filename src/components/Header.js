61d5b794af07543481bb73e43cf057ce18501e58
pabloa1311 <pabloa1311@gmail.com>
2020-12-30 15:17:33 -0500

    Header and Cart page corrected

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React from "react"
+import React, {useContext} from "react"
 import {Link} from "react-router-dom"
+import {Context} from "../Context"
 
 function Header() {
+    const {cartItems} = useContext(Context)
+    
+    function cartHeader() {
+        if (cartItems.length > 0) {
+            return <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+        } else {
+            return <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        }
+    }
+    
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">
+                {cartHeader()}
+            </Link>
         </header>
     )
 }
